86f73079e51367f4dc63b480d1ff9c9c9a2be144
dineshetouch <47388567+dineshetouch@users.noreply.github.com>
2019-02-22 20:42:46 +0530

    Add files via upload

--- Arrays/swapFirstLastElementArray.java
+++ Arrays/swapFirstLastElementArray.java
@@ -1,32 +1,31 @@
 package Array;
 
+//Write a Java program to swap the first and last elements of an array (length must be at least 1) and create a new array
 public class swapFirstLastElementArray {
 
 	public static void main(String[] args) {
-		
-		int i[] = {11, 12, 13, 14, 15, 16, 17};
-		
-		/*System.out.println(i.length);
-		System.out.println(i[0]);
-		System.out.println(i[i.length-1]);*/
-		
-		for (int a = 0; a<i.length; a++) {
-			System.out.print(i[a]+" ");
+
+		int arr[] = { 11, 12, 13, 14, 15, 16, 17 };
+
+		// print the given array
+		for (int a = 0; a < arr.length; a++) {
+			System.out.print(arr[a] + " ");
 		}
-		
+
 		System.out.println();
-		int temp;		
-		temp = i[0];
-		i[0] = i[i.length-1];
-		i[i.length-1] = temp;
-		
-		/*System.out.println(i[0]);
-		System.out.println(i[i.length-1]);*/
-		
-		for (int a = 0; a<i.length; a++) {
-			System.out.print(i[a]+" ");
+
+		int temp; // initialize the temp variable
+
+		// swap first and last element of integer array
+		temp = arr[0];
+		arr[0] = arr[arr.length - 1]; // swap the first element
+		arr[arr.length - 1] = temp; // swap the last element
+
+		// Print the new array after swapping
+		for (int a = 0; a < arr.length; a++) {
+			System.out.print(arr[a] + " ");
 		}
-		
+
 	}
 
 }
