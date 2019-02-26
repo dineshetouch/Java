d5944e3bf45688bfad19efa9b0da47488918ca11
dineshetouch <47388567+dineshetouch@users.noreply.github.com>
2019-02-26 20:34:01 +0530

    Add files via upload

--- Arrays/environmentVariable.java
+++ Arrays/environmentVariable.java
@@ -1,4 +1,7 @@
 package Array;
+
+import java.util.Map;
+
 //Write a Java program to get the value of the environment variable PATH, TEMP, USERNAME. 
 public class environmentVariable {
 
@@ -7,21 +10,29 @@ public class environmentVariable {
 		//System.getenv() is working on key value pair (HashMap)
 		//Will pass the key name in System.getenv("key name") and will get their corresponding value.
 		
-		System.out.println(System.getenv("Path")); //give the path of eclipse
-		System.out.println(System.getenv("TEMP")); // give the path of temp file
-		System.out.println(System.getenv("Username")); // give the path of current OS username
-		System.out.println(System.getenv("OS")); // provide OS details
-		System.out.println(System.getenv("PATHEXT")); 
-		System.out.println(System.getenv("pROCESSOR_IDENTIFIER")); // key value in given string is not case sensitive.
-		System.out.println(System.getenv("windir"));
-		System.out.println(System.getenv("windows_tracing_flags"));
-		System.out.println(System.getenv("PSMOdulePath"));
-		System.out.println(System.getenv("NUMBER_OF_PROCESSORS"));
-		System.out.println(System.getenv("PROCESSOR_LEVEL"));
-
+		Map<String, String> map = System.getenv();
 		
-		 //getting null value as system didn't get the "Environment variable Path" key.
-		System.out.println(System.getenv("Environment variable Path"));
+		for(String i: map.keySet()) {
+			System.out.println(i + " = "+ System.getenv(i));
+			//System.out.println(System.getenv(i)); 
+			
+		}
+		System.out.println("-----------------------------");
+//		System.out.println(System.getenv("Path")); //give the path of eclipse
+//		System.out.println(System.getenv("TEMP")); // give the path of temp file
+//		System.out.println(System.getenv("Username")); // give the path of current OS username
+//		System.out.println(System.getenv("OS")); // provide OS details
+//		System.out.println(System.getenv("PATHEXT")); 
+//		System.out.println(System.getenv("pROCESSOR_IDENTIFIER")); // key value in given string is not case sensitive.
+//		System.out.println(System.getenv("windir"));
+//		System.out.println(System.getenv("windows_tracing_flags"));
+//		System.out.println(System.getenv("PSMOdulePath"));
+//		System.out.println(System.getenv("NUMBER_OF_PROCESSORS"));
+//		System.out.println(System.getenv("PROCESSOR_LEVEL"));
+//
+//		
+//		 //getting null value as system didn't get the "Environment variable Path" key.
+//		System.out.println(System.getenv("Environment variable Path"));
 		
 		
 	}
